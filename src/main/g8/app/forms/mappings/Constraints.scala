fbd7566b8eb257ccd367913442586b7e17334cb9
Nick Wilson <thenickwilson@gmail.com>
2017-12-06 10:53:42 +0000

    Add constraints
    Created some useful constraints for common validation concerns, including
    min and max values, maximum string length, and regular expressions.

--- src/main/g8/app/forms/mappings/Constraints.scala
+++ src/main/g8/app/forms/mappings/Constraints.scala
@@ -4,6 +4,15 @@ import play.api.data.validation.{Constraint, Invalid, Valid}
 
 trait Constraints {
 
+  protected def firstError[A](constraints: Constraint[A]*): Constraint[A] =
+    Constraint {
+      input =>
+        constraints
+          .map(_.apply(input))
+          .find(_ != Valid)
+          .getOrElse(Valid)
+    }
+
   protected def minimumValue[A](minimum: A, errorKey: String)(implicit ev: Ordering[A]): Constraint[A] =
     Constraint {
       input =>
@@ -15,5 +24,34 @@ trait Constraints {
         } else {
           Invalid(errorKey, minimum)
         }
+    }
+
+  protected def maximumValue[A](maximum: A, errorKey: String)(implicit ev: Ordering[A]): Constraint[A] =
+    Constraint {
+      input =>
+
+        import ev._
+
+        if (input <= maximum) {
+          Valid
+        } else {
+          Invalid(errorKey, maximum)
+        }
+    }
+
+  protected def regexp(regex: String, errorKey: String): Constraint[String] =
+    Constraint {
+      case str if str.matches(regex) =>
+        Valid
+      case _ =>
+        Invalid(errorKey, regex)
+    }
+
+  protected def maxLength(maximum: Int, errorKey: String): Constraint[String] =
+    Constraint {
+      case str if str.length <= maximum =>
+        Valid
+      case _ =>
+        Invalid(errorKey, maximum)
     }
 }
