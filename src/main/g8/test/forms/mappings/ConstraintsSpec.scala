3f1b10a5b2fca0a6fc636892126884f580fe7471
Nick Wilson <thenickwilson@gmail.com>
2017-12-06 10:53:42 +0000

    Add constraints
    Created some useful constraints for common validation concerns, including
    min and max values, maximum string length, and regular expressions.

--- src/main/g8/test/forms/mappings/ConstraintsSpec.scala
+++ src/main/g8/test/forms/mappings/ConstraintsSpec.scala
@@ -5,6 +5,30 @@ import play.api.data.validation.{Invalid, Valid}
 
 class ConstraintsSpec extends WordSpec with MustMatchers with Constraints {
 
+
+  "firstError" must {
+
+    "return Valid when all constraints pass" in {
+      val result = firstError(maxLength(10, "error.length"), regexp("""^\w+\$""", "error.regexp"))("foo")
+      result mustEqual Valid
+    }
+
+    "return Invalid when the first constraint fails" in {
+      val result = firstError(maxLength(10, "error.length"), regexp("""^\w+\$""", "error.regexp"))("a" * 11)
+      result mustEqual Invalid("error.length", 10)
+    }
+
+    "return Invalid when the second constraint fails" in {
+      val result = firstError(maxLength(10, "error.length"), regexp("""^\w+\$""", "error.regexp"))("")
+      result mustEqual Invalid("error.regexp", """^\w+\$""")
+    }
+
+    "return Invalid for the first error when both constraints fail" in {
+      val result = firstError(maxLength(-1, "error.length"), regexp("""^\w+\$""", "error.regexp"))("")
+      result mustEqual Invalid("error.length", -1)
+    }
+  }
+
   "minimumValue" must {
 
     "return Valid for a number greater than the threshold" in {
@@ -20,6 +44,60 @@ class ConstraintsSpec extends WordSpec with MustMatchers with Constraints {
     "return Invalid for a number below the threshold" in {
       val result = minimumValue(1, "error.min").apply(0)
       result mustEqual Invalid("error.min", 1)
+    }
+  }
+
+  "maximumValue" must {
+
+    "return Valid for a number less than the threshold" in {
+      val result = maximumValue(1, "error.max").apply(0)
+      result mustEqual Valid
+    }
+
+    "return Valid for a number equal to the threshold" in {
+      val result = maximumValue(1, "error.max").apply(1)
+      result mustEqual Valid
+    }
+
+    "return Invalid for a number above the threshold" in {
+      val result = maximumValue(1, "error.max").apply(2)
+      result mustEqual Invalid("error.max", 1)
+    }
+  }
+
+  "regexp" must {
+
+    "return Valid for an input that matches the expression" in {
+      val result = regexp("""^\w+\$""", "error.invalid")("foo")
+      result mustEqual Valid
+    }
+
+    "return Invalid for an input that does not match the expression" in {
+      val result = regexp("""^\d+\$""", "error.invalid")("foo")
+      result mustEqual Invalid("error.invalid", """^\d+\$""")
+    }
+  }
+
+  "maxLength" must {
+
+    "return Valid for a string shorter than the allowed length" in {
+      val result = maxLength(10, "error.length")("a" * 9)
+      result mustEqual Valid
+    }
+
+    "return Valid for an empty string" in {
+      val result = maxLength(10, "error.length")("")
+      result mustEqual Valid
+    }
+
+    "return Valid for a string equal to the allowed length" in {
+      val result = maxLength(10, "error.length")("a" * 10)
+      result mustEqual Valid
+    }
+
+    "return Invalid for a string longer than the allowed length" in {
+      val result = maxLength(10, "error.length")("a" * 11)
+      result mustEqual Invalid("error.length", 10)
     }
   }
 }
