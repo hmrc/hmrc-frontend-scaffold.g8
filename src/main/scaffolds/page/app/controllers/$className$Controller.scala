b02d34009ef725df3f7b9eee1d3633ee84072e30
Nick Wilson <thenickwilson@gmail.com>
2017-12-21 11:31:41 +0000

    Remove redundant imports in page scaffold

--- src/main/scaffolds/page/app/controllers/$className$Controller.scala
+++ src/main/scaffolds/page/app/controllers/$className$Controller.scala
@@ -2,15 +2,10 @@ package controllers
 
 import javax.inject.Inject
 
-import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
-import connectors.DataCacheConnector
 import controllers.actions._
 import config.FrontendAppConfig
-import forms.BooleanForm
-import models.Mode
-import utils.{Navigator, UserAnswers}
 import views.html.$className;format="decap"$
 
 import scala.concurrent.Future
