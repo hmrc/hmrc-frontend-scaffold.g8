d4741fe271761290ce1feb23649ef60fab79116f
Nick Wilson <thenickwilson@gmail.com>
2018-01-04 13:33:13 +0000

    Use property testing for int forms
    Changed the spec for int page forms to use property-based testing
    rather than specific values, as this gives us far higher levels
    of confidence.
    
    Changed the intPage scaffold itself to prompt the use for a min
    and max value for the page.

--- src/main/g8/project/FrontendBuild.scala
+++ src/main/g8/project/FrontendBuild.scala
@@ -28,6 +28,7 @@ private object AppDependencies {
   private val playConditionalFormMappingVersion = "0.2.0"
   private val playLanguageVersion = "3.4.0"
   private val bootstrapVersion = "1.0.0"
+  private val scalacheckVersion = "1.13.4"
 
   val compile = Seq(
     ws,
@@ -56,7 +57,8 @@ private object AppDependencies {
         "org.pegdown" % "pegdown" % pegdownVersion % scope,
         "org.jsoup" % "jsoup" % "1.10.3" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "org.mockito" % "mockito-all" % mockitoAllVersion % scope
+        "org.mockito" % "mockito-all" % mockitoAllVersion % scope,
+        "org.scalacheck" %% "scalacheck" % scalacheckVersion % scope
       )
     }.test
   }
