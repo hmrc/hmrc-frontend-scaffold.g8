7394cfd5851a3154286cd09cf4d5df3871c2f096
Nick Wilson <thenickwilson@gmail.com>
2018-01-09 16:19:54 +0000

    Use property testing for options page form

---
+++ src/main/g8/test/forms/behaviours/OptionFieldBehaviours.scala
@@ -0,0 +1,34 @@
+package forms.behaviours
+
+import play.api.data.{Form, FormError}
+
+class OptionFieldBehaviours extends FieldBehaviours {
+
+  def optionsField[T](form: Form[_],
+                      fieldName: String,
+                      validValues: Set[T],
+                      invalidError: FormError): Unit = {
+
+
+    "bind all valid values" in {
+
+      for(value <- validValues) {
+
+        val result = form.bind(Map(fieldName -> value.toString)).apply(fieldName)
+        result.value.value shouldEqual value.toString
+      }
+    }
+
+    "not bind invalid values" in {
+
+      val generator = stringsExceptSpecificValues(validValues.map(_.toString))
+
+      forAll(generator -> "invalidValue") {
+        value =>
+
+          val result = form.bind(Map(fieldName -> value)).apply(fieldName)
+          result.errors shouldEqual Seq(invalidError)
+      }
+    }
+  }
+}
