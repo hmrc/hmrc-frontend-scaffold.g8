9ca4aa9590684bc0eeaafa899236b5fb6e35ef66
MarkHunter2 <mark.hunter2@digital.hmrc.gov.uk>
2018-06-12 17:02:42 +0100

    Back link : Prevent resubmit warnings, and guard against host change.

--- src/main/g8/app/assets/javascripts/$name__word$.js
+++ src/main/g8/app/assets/javascripts/$name__word$.js
@@ -16,9 +16,19 @@
   // =====================================================
   // Back link mimics browser back functionality
   // =====================================================
+  // store referrer value to cater for IE - https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/10474810/  */
+  var docReferrer = document.referrer
+  // prevent resubmit warning
+  if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
+    window.history.replaceState(null, null, window.location.href);
+  }
+  // back click handle, dependent upon presence of referrer & no host change
   \$('#back-link').on('click', function(e){
     e.preventDefault();
-    window.history.back();
+    if (window.history && window.history.back && typeof window.history.back === 'function' &&
+       (docReferrer !== "" && docReferrer.indexOf(window.location.host) !== -1)) {
+        window.history.back();
+    }
   })
 
   // =====================================================
