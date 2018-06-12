26cfc953a0ac51903ae0672e8ce75cbe5281c4ff
MarkHunter2 <mark.hunter2@digital.hmrc.gov.uk>
2018-06-12 17:30:22 +0100

    Individual field errors : Bring styling in line with latest GOV Elements.

--- src/main/g8/test/views/behaviours/IntViewBehaviours.scala
+++ src/main/g8/test/views/behaviours/IntViewBehaviours.scala
@@ -41,7 +41,7 @@ trait IntViewBehaviours extends QuestionViewBehaviours[Int] {
 
         "show an error in the value field's label" in {
           val doc = asDocument(createView(form.withError(error)))
-          val errorSpan = doc.getElementsByClass("error-notification").first
+          val errorSpan = doc.getElementsByClass("error-message").first
           errorSpan.text mustBe messages(errorMessage)
         }
       }
--- src/main/g8/test/views/behaviours/YesNoViewBehaviours.scala
+++ src/main/g8/test/views/behaviours/YesNoViewBehaviours.scala
@@ -52,7 +52,7 @@ trait YesNoViewBehaviours extends QuestionViewBehaviours[Boolean] {
 
         "show an error in the value field's label" in {
           val doc = asDocument(createView(form.withError(error)))
-          val errorSpan = doc.getElementsByClass("error-notification").first
+          val errorSpan = doc.getElementsByClass("error-message").first
           errorSpan.text mustBe messages(errorMessage)
         }
       }
