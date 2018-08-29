b40a2b2bb57cce29cc4e06d80e77ffcba8d8224c
Nick Wilson <thenickwilson@gmail.com>
2018-08-29 08:08:07 +0100

    Bump library and plugin versions

--- src/main/g8/project/MicroService.scala
+++ src/main/g8/project/MicroService.scala
@@ -57,7 +57,6 @@ trait MicroService {
       Keys.fork in IntegrationTest := false,
       unmanagedSourceDirectories in IntegrationTest <<= (baseDirectory in IntegrationTest)(base => Seq(base / "it")),
       addTestReportOption(IntegrationTest, "int-test-reports"),
-      testGrouping in IntegrationTest := oneForkedJvmPerTest((definedTests in IntegrationTest).value),
       parallelExecution in IntegrationTest := false)
       .settings(resolvers ++= Seq(
         Resolver.bintrayRepo("hmrc", "releases"),
