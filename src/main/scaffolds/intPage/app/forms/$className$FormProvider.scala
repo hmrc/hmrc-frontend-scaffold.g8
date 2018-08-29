135661730ba6df8ac379ce9b8a3d4fb610599de2
Nick Wilson <thenickwilson@gmail.com>
2018-08-29 08:17:05 +0100

    Remove redundant FormErrorHelper

--- src/main/scaffolds/intPage/app/forms/$className$FormProvider.scala
+++ src/main/scaffolds/intPage/app/forms/$className$FormProvider.scala
@@ -5,7 +5,7 @@ import javax.inject.Inject
 import forms.mappings.Mappings
 import play.api.data.Form
 
-class $className$FormProvider @Inject() extends FormErrorHelper with Mappings {
+class $className$FormProvider @Inject() extends Mappings {
 
   def apply(): Form[Int] =
     Form(
--- src/main/scaffolds/optionsPage/app/forms/$className$FormProvider.scala
+++ src/main/scaffolds/optionsPage/app/forms/$className$FormProvider.scala
@@ -6,7 +6,7 @@ import forms.mappings.Mappings
 import play.api.data.Form
 import models.$className$
 
-class $className$FormProvider @Inject() extends FormErrorHelper with Mappings {
+class $className$FormProvider @Inject() extends Mappings {
 
   def apply(): Form[$className$] =
     Form(
