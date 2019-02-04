4c2746c837a98e7fd4df27479d643bbe7bd10501
Thomas Pascoe <15924950+Pascoe121@users.noreply.github.com>
2019-02-04 08:00:11 +0000

    add messages to CYA helper

--- src/main/g8/app/utils/CheckYourAnswersHelper.scala
+++ src/main/g8/app/utils/CheckYourAnswersHelper.scala
@@ -3,7 +3,8 @@ package utils
 import controllers.routes
 import models.{CheckMode, UserAnswers}
 import pages._
+import play.api.i18n.Messages
 import viewmodels.{AnswerRow, RepeaterAnswerRow, RepeaterAnswerSection}
 
-class CheckYourAnswersHelper(userAnswers: UserAnswers) {
+class CheckYourAnswersHelper(userAnswers: UserAnswers)(implicit messages: Messages) {
 }
