38d01673f28af0030e93f49fad3ab391d31a3be3
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2019-11-22 14:11:32 +0000

    Update dependencies for Nunjuck support
    Change from the spike libraries to use the new libraries for `play-nunjucks` and `play-nunjucks-viewmodel`

--- src/main/g8/project/AppDependencies.scala
+++ src/main/g8/project/AppDependencies.scala
@@ -11,8 +11,8 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "0.2.0",
     "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.0.0",
     "uk.gov.hmrc"       %% "play-whitelist-filter"          % "2.0.0",
-    "uk.gov.hmrc"       %% "play-nunjucks-spike"            % "0.12.0-play-26",
-    "uk.gov.hmrc"       %% "play-nunjucks-viewmodel-spike"  % "0.1.0-SNAPSHOT",
+    "uk.gov.hmrc"       %% "play-nunjucks"                  % "0.17.0-play-26",
+    "uk.gov.hmrc"       %% "play-nunjucks-viewmodel"        % "0.4.0",
     "org.webjars.npm"   %  "govuk-frontend"                 % "3.1.0"
   )
 
